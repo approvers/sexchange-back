635b4422f4c7410a9efdabf6ebb3ce6d16613385
FlexiblePrintedCircuits <yugehayata.dev@gmail.com>
2020-10-10 01:57:43 +0900

    UPDATE: ユーザーリスト表示機能を実装

--- main.py
+++ main.py
@@ -69,7 +69,7 @@ def exchange():
 
     try:
         cursor.execute(
-            "insert into exchange values (?, {}, {})".format(twitter_id_1, twitter_id_2)
+            "insert into exchange values (?, {}, {});".format(twitter_id_1, twitter_id_2)
         )
         connection.commit()
         connection.close()
@@ -80,6 +80,38 @@ def exchange():
         return jsonify({"status": 0})
 
 
+@app.route('/users', methods=['GET'])
+def users():
+    twitter_id = request.args.get('twitter_id', '')
+    users_dict = []
+
+    connection = sqlite3.connect(dbpath)
+    cursor = connection.cursor()
+
+    cursor.execute(
+        "select * from exchange where twitter_id_1={};".format(twitter_id)
+    )
+
+    if cursor.fetchone() == None:
+        return jsonify({"users": []})
+    else:
+        res = cursor.fetchall()
+        for i in res:
+            cursor.execute(
+                "select name, twitter_id, seiheki from users where twitter_id={};".format(i[0])
+            )
+            user_info = cursor.fetchone()
+            users_dict.append(
+                {
+                    "name": user_info[0],
+                    "twitter_id": user_info[1],
+                    "seiheki": user_info[2]
+                }
+            )
+        
+        return jsonify({"users": [users_dict]})
+                
+            
 
 if __name__ == '__main__':
     app.run(debug = True)
