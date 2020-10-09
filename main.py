b65ba3c53d57fd4e16a3cbf69fd9c92aef6843be
FlexiblePrintedCircuits <yugehayata.dev@gmail.com>
2020-10-10 01:30:44 +0900

    UPDATE: ログイン機能を実装

--- main.py
+++ main.py
@@ -35,5 +35,29 @@ def register():
         return jsonify({"status": 0})
 
 
+@app.route('/login', methods=['POST'])
+def login():
+    twitter_id = request.form("twitter_id")
+    password = request.form("password")
+    
+    connection = sqlite3.connect(dbpath)
+    cursor = connection.cursor()
+
+    cursor.execute(
+        "select * from users where twitter_id={} and password={};".format(twitter_id, password)
+    )
+
+    res = cursor.fetchone()
+    if res == None:
+        connection.commit()
+        connection.close()
+        return jsonify({"status": 0})    
+    else:
+        connection.commit()
+        connection.close()
+        return jsonify({"status": 1})
+
+
+
 if __name__ == '__main__':
     app.run(debug = True)
